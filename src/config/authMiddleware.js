0180eb84cee607900510b5549be209dd7869dca9
Victor Ulloa <alejandroekas@gmail.com>
2024-11-14 02:03:32 -0500

    created middleware with jsonwebtoken install

---
+++ src/config/authMiddleware.js
@@ -0,0 +1,20 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET || 'your_secret_key';
+
+function verifyToken(req, res, next) {
+    const token = req.headers['authorization'];
+    if (!token) {
+        return res.status(403).json({ message: 'No token provided' });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Failed to authenticate token' });
+        }
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+module.exports = verifyToken;
